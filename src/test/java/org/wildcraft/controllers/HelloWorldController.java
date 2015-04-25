59c8c8d93386fbacefc88a496cd1699e3d1544ee
Narendran Solai <narendran.ss@gmail.com>
2015-04-25 22:03:18 +0530

    Simple REST Based Boot Service
    Simple REST Based Boot Service - Hello World Service

---
+++ src/test/java/org/wildcraft/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.wildcraft.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/sayHello")
+	public @ResponseBody String sayHello() {
+		return "Hello World!!!";
+	}
+}
